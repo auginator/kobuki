e74e2c2f1b9d611f99601b825af0316389cadba3
Agustin Sevilla <amsevilla3@gmail.com>
2026-01-10 15:52:22 -0800

    Try launchfile for rplidar. Move transform there

--- kobuki/launch/augs_kobuki_node.launch.py
+++ kobuki/launch/augs_kobuki_node.launch.py
@@ -25,21 +25,9 @@ def generate_launch_description():
         )
     )
 
-    # Static transform publisher from base_link to laser
-    static_transform_publisher = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='base_link_to_laser_transform',
-        arguments=['--x', '0', '--y', '0', '--z', '0',
-                   '--roll', '0', '--pitch', '0', '--yaw', '0',
-                   '--frame-id', 'base_link', '--child-frame-id', 'laser'],
-        output='screen'
-    )
-
     ld = LaunchDescription()
 
     ld.add_action(kobuki_node_launch)
     ld.add_action(kobuki_description_launch)
-    ld.add_action(static_transform_publisher)
 
     return ld
