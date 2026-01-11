4366561a1c58a2cf26ab68bcc255147d049fcab8
Agustin Sevilla <amsevilla3@gmail.com>
2026-01-10 16:02:57 -0800

    Split out a dedicated slam ROS2 package.

--- slam.Dockerfile
+++ slam.Dockerfile
@@ -27,13 +27,16 @@ RUN apt-get update && \
 RUN mkdir -p src && \
   git clone https://github.com/slamtec/sllidar_ros2.git src/sllidar_ros2
 
+# Copy the slam package into the workspace
+COPY slam src/slam
+
 # Install dependencies for the cloned package
 RUN . /opt/ros/humble/setup.sh && \
   rosdep install -i --from-path src --rosdistro humble -y
 
 # Build the workspace
-RUN cd src && . /opt/ros/humble/setup.sh  && \
-  cd sllidar_ros2 && colcon build --symlink-install
+RUN . /opt/ros/humble/setup.sh && \
+  colcon build --symlink-install
 
 # Set the entrypoint to source the workspace and run the node
-CMD ["/bin/bash", "-c", ". /opt/ros/humble/setup.sh && cd src/sllidar_ros2 && . ./install/setup.bash && ros2 launch sllidar_ros2 sllidar_c1_launch.py"]
+CMD ["/bin/bash", "-c", ". /opt/ros/humble/setup.sh && . ./install/setup.bash && ros2 launch slam sllidar_with_transform.launch.py"]
---
+++ slam/launch/sllidar_with_transform.launch.py
@@ -0,0 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # Include the sllidar_ros2 launch file
+    sllidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory(
+                'sllidar_ros2'), 'launch', 'sllidar_c1_launch.py')
+        )
+    )
+
+    # Static transform publisher from base_link to laser
+    static_transform_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_link_to_laser_transform',
+        arguments=['--x', '0', '--y', '0', '--z', '0',
+                   '--roll', '0', '--pitch', '0', '--yaw', '0',
+                   '--frame-id', 'base_link', '--child-frame-id', 'laser'],
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(sllidar_launch)
+    ld.add_action(static_transform_publisher)
+
+    return ld
