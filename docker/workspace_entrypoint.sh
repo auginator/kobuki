241a933581a34dfb71dace942065f8d24030b342
Agustin Sevilla <amsevilla3@gmail.com>
2026-01-14 23:06:59 -0800

    fix race condition requiring unplugging the base and plugging it back in. Wait for udev to process rules and create symlink in workspace_entrypoint

--- docker/workspace_entrypoint.sh
+++ docker/workspace_entrypoint.sh
@@ -3,7 +3,26 @@ set -e
 
 # start udev
 /lib/systemd/systemd-udevd --daemon
-    
+
+# Wait for udev to process rules and create the kobuki symlink
+echo "Waiting for Kobuki device..."
+timeout=30
+counter=0
+while [ ! -e /dev/kobuki ] && [ $counter -lt $timeout ]; do
+    sleep 1
+    counter=$((counter + 1))
+done
+
+if [ -e /dev/kobuki ]; then
+    echo "Kobuki device found at /dev/kobuki"
+else
+    echo "WARNING: Kobuki device not found after ${timeout}s"
+fi
+
+# Trigger udev to process any pending events
+udevadm trigger
+udevadm settle
+
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "$WORKSPACE_ROOT/install/setup.bash"
