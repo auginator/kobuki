5828c09cb968f951c23ea87171e212075fbd7093
Agustin Sevilla <amsevilla3@gmail.com>
2026-01-17 00:54:04 -0800

    remove extra transform publisher

--- kobuki/launch/slam.launch.py
+++ kobuki/launch/slam.launch.py
@@ -17,20 +17,8 @@ def generate_launch_description():
         )
     )
 
-    # Static transform publisher from base_link to laser
-    static_transform_publisher = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='base_link_to_laser_transform',
-        arguments=['--x', '0', '--y', '0', '--z', '0',
-                   '--roll', '0', '--pitch', '0', '--yaw', '0',
-                   '--frame-id', 'base_link', '--child-frame-id', 'laser'],
-        output='screen'
-    )
-
     ld = LaunchDescription()
 
     ld.add_action(sllidar_launch)
-    ld.add_action(static_transform_publisher)
 
     return ld
